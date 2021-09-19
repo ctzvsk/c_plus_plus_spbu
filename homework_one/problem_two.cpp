04f01dcecf0cacb5e55ab2b28937d288f2f1e099
ctzvsk <78962241+ctzvsk@users.noreply.github.com>
2021-09-19 23:54:37 +0300

    Update problem_two.cpp

--- homework_one/problem_two.cpp
+++ homework_one/problem_two.cpp
@@ -1,33 +1,23 @@
 #include <iostream>
-#include <string>
 using namespace std;
 int main()
 {
-	string mm_str, ss_str = "";
 	int h = 0, mm = 0, ss = 0, totals = 0;
 	cout << "type in amount of seconds" << endl;
 	cin >> totals;
 	h = totals / 3600;
 	mm = (totals - 3600 * h) / 60; 
 	ss = (totals - 3600 * h) % 60;
+	cout << h << ":";
 	if (mm < 10)
 	{
-		mm_str = to_string(mm);
-		mm_str.insert(0, "0");
+		cout << "0";
 	}
-	else
-	{
-		mm_str = to_string(mm);
-	}
+	cout << mm;
 	if (ss < 10)
 	{
-		ss_str = to_string(ss);
-		ss_str.insert(0, "0");
+		cout << "0";
 	}
-	else
-	{
-		ss_str = to_string(ss);
-	}
-	cout << h << ":" << mm_str << ":" << ss_str << endl;
+	cout << ss << endl;
 	return 0;
 }
