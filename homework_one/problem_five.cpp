e6a40f55ed3a352260e83a564544428e453c47fe
ctzvsk <78962241+ctzvsk@users.noreply.github.com>
2021-09-21 23:36:25 +0300

    Update problem_five.cpp

--- homework_one/problem_five.cpp
+++ homework_one/problem_five.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
-	int n, m = 0;
+	int n = 0;
+	int m = 0;
 	cout << "type in two integer numbers" << endl;
 	cin >> n >> m;
 	cout << m % n % m + n % m % n + 1 << endl;
