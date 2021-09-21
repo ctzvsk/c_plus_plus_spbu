f9e7582d2c864df3fa612b3c391cbff8262c6f87
ctzvsk <78962241+ctzvsk@users.noreply.github.com>
2021-09-21 23:35:01 +0300

    Update problem_one.cpp

--- homework_one/problem_one.cpp
+++ homework_one/problem_one.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main()
 {
 	int point = 0;
-	int V, T;
+	int V = 0;
+	int T = 0;
 	cout << "Type in speed and time: " << endl;
 	cin >> V >> T;
 	point = (109 + V * T % 109) % 109;
