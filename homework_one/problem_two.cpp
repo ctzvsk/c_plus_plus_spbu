e4be21d15f41126f61fea982657b560f602eb4b1
ctzvsk <78962241+ctzvsk@users.noreply.github.com>
2021-09-21 14:27:05 +0300

    Update problem_two.cpp

--- homework_one/problem_two.cpp
+++ homework_one/problem_two.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
@@ -6,13 +7,11 @@ int main()
 	cout << "type in amount of seconds" << endl;
 	cin >> totals;
 	h = totals / 3600;
-	mm = (totals - 3600 * h) / 60; 
+	mm = (totals - 3600 * h) / 60;
 	ss = (totals - 3600 * h) % 60;
 	cout << h << ":";
-	if (mm < 10)
-	{
-		cout << "0";
-	}
+	cout << setfill('0') << setw(2) << mm << ':';
+	cout << setfill('0') << setw(2) << ss << endl;
 	cout << mm;
 	if (ss < 10)
 	{
