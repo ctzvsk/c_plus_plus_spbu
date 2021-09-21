a67f5051d554ef3fae85d6469eb3097ed4744f73
ctzvsk <78962241+ctzvsk@users.noreply.github.com>
2021-09-21 14:31:14 +0300

    Update problem_three.cpp

--- homework_one/problem_three.cpp
+++ homework_one/problem_three.cpp
@@ -5,14 +5,7 @@ int main()
 	int d, h, a, b = 0;
 	cout << "type in height of the pole, amount of meters rising each day and amount of meters descending each day" << endl;
 	cin >> h >> a >> b;
-	if (b < a)
-	{
-		d = h / (a - b);
-		cout << "The snail will rise to the top of the pole in " << d << " days" << endl;
-	}
-	else
-	{
-		cout << "The snail will never rise to the top of the pole" << endl;
-	}
+	d = h / (a - b);
+	cout << "The snail will rise to the top of the pole in " << d << " days" << endl;
 	return 0;
 }
