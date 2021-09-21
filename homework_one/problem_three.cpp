cc5bbded531a6a9016bf4e2f4923bb4835c49570
ctzvsk <78962241+ctzvsk@users.noreply.github.com>
2021-09-21 23:34:31 +0300

    Update problem_three.cpp

--- homework_one/problem_three.cpp
+++ homework_one/problem_three.cpp
@@ -8,7 +8,7 @@ int main()
 	int b = 0;
 	cout << "type in height of the pole, amount of meters rising each day and amount of meters descending each day" << endl;
 	cin >> h >> a >> b;
-	d = h / (a - b);
+	d = (h - 2 * b + a - 1) / (a - b);
 	cout << "The snail will rise to the top of the pole in " << d << " days" << endl;
 	return 0;
 }
