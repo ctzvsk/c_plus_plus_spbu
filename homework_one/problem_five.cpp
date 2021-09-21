1eea02cd5f0a788161dfaf2ae2fdb9c56a45c853
ctzvsk <78962241+ctzvsk@users.noreply.github.com>
2021-09-21 14:41:19 +0300

    Update problem_five.cpp

--- homework_one/problem_five.cpp
+++ homework_one/problem_five.cpp
@@ -5,13 +5,6 @@ int main()
 	int n, m = 0;
 	cout << "type in two integer numbers" << endl;
 	cin >> n >> m;
-	if ((n % m == 0) or (m % n == 0))
-	{
-		cout << "1" << endl;
-	}
-	else
-	{
-		cout << "2" << endl;
-	}
+	cout << m % n % m + n % m % n + 1 << endl;
 	return 0;
 }
